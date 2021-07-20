e1318a681aef4382f517f3fed34186b91cfd5a4a
michael-gerstenberg <michael@gerstenberg.ch>
2021-07-20 18:05:58 +0200

    connectors added

--- connectors/04_mongo_customers.sh
+++ connectors/04_mongo_customers.sh
@@ -1,12 +1,12 @@
-curl -X PUT http://localhost:8083/connectors/mongo-sink-orders/config -H "Content-Type: application/json" -d ' {
+curl -X PUT http://localhost:8083/connectors/mongo-sink-customers/config -H "Content-Type: application/json" -d ' {
         "connector.class":"com.mongodb.kafka.connect.MongoSinkConnector",
         "tasks.max":"1",
-        "topics":"oracle-bulk-ORDERS",
-        "connection.uri":"mongodb+srv://<username>:<password>@<endpoint>",
-        "database":"FromOracle",
-        "collection":"orders",
+        "topics":"oracle-bulk-CUSTOMERS",
+        "connection.uri":"mongodb+srv://michael:1wZYT318g49o5cpL@cluster0.da1ep.mongodb.net",
+        "database":"TEDDY",
+        "collection":"customers",
         "document.id.strategy":"com.mongodb.kafka.connect.sink.processor.id.strategy.PartialValueStrategy",
-        "document.id.strategy.partial.value.projection.list":"ORDER_ID",
+        "document.id.strategy.partial.value.projection.list":"CUSTOMER_ID",
         "document.id.strategy.partial.value.projection.type":"AllowList",
         "writemodel.strategy":"com.mongodb.kafka.connect.sink.writemodel.strategy.UpdateOneBusinessKeyTimestampStrategy"
 }'
